03453b98b95da864ffbfdf6a53190db641a5d043
RustamAleev2023 <ridermix@yandex.ru>
2023-11-23 19:48:40 +0200

    Tasks4,5 done

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,9 @@ public class Main {
     public static void main(String[] args) {
 //        task1();
 //        task2();
-        task3();
+//        task3();
+//        task4();
+        task5();
     }
 
     //Task1
@@ -21,16 +23,16 @@ public class Main {
     }
 
     //Task2
-    public static void task2(){
+    public static void task2() {
         System.out.println("Введите размер стороны квадрата");
         Scanner scanner = new Scanner(System.in);
         int n = scanner.nextInt();
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
-                if(i == 0 || i == n-1){
+                if (i == 0 || i == n - 1) {
                     System.out.print("*");
                 } else {
-                    if(j == 0 || j == n - 1){
+                    if (j == 0 || j == n - 1) {
                         System.out.print("*");
                     } else {
                         System.out.print(" ");
@@ -42,7 +44,7 @@ public class Main {
     }
 
     //Task3
-    public static void task3(){
+    public static void task3() {
         System.out.println("Введите длину прямоугольника");
         Scanner scanner = new Scanner(System.in);
         int l = scanner.nextInt();
@@ -51,15 +53,51 @@ public class Main {
 
         for (int i = 0; i < h; i++) {
             for (int j = 0; j < l; j++) {
-                if(i == 0 || i == h-1){
+                if (i == 0 || i == h - 1) {
                     System.out.print("*");
                 } else {
-                    if(j == 0 || j == l - 1){
+                    if (j == 0 || j == l - 1) {
                         System.out.print("*");
                     } else {
                         System.out.print(" ");
                     }
                 }
+            }
+            System.out.println("");
+        }
+    }
+
+    //Task4
+    public static void task4() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите положительное число");
+        int n = scanner.nextInt();
+
+        for (int i = 1; i <= n; i++) {
+            boolean isPrime = true;
+            for (int j = 2; j <= (int) Math.sqrt(i); j++) {
+                if ((i % j) == 0) {
+                    isPrime = false;
+                    break;
+                }
+            }
+            if (isPrime) {
+                System.out.println(i);
+            }
+        }
+    }
+
+    //Task5
+    public static void task5() {
+        System.out.println("Введите нижнюю границу диапазона");
+        Scanner scanner = new Scanner(System.in);
+        int min = scanner.nextInt();
+        System.out.println("Введите верхнюю границу диапазона");
+        int max = scanner.nextInt();
+
+        for (int i = min; i <= max; i++) {
+            for (int j = 1; j <= 10; j++) {
+                System.out.print(i + "*" + j + " = " + i * j + " ");
             }
             System.out.println("");
         }
