726669909f43c27dc5a0d9b3a825f9260d097077
RustamAleev2023 <ridermix@yandex.ru>
2023-11-23 19:21:11 +0200

    Task2 done

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,8 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        task1();
+//        task1();
+        task2();
     }
 
     //Task1
@@ -13,6 +14,27 @@ public class Main {
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
                 System.out.print("*");
+            }
+            System.out.println("");
+        }
+    }
+
+    //Task2
+    public static void task2(){
+        System.out.println("Введите размер стороны квадрата");
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if(i == 0 || i == n-1){
+                    System.out.print("*");
+                } else {
+                    if(j == 0 || j == n - 1){
+                        System.out.print("*");
+                    } else {
+                        System.out.print(" ");
+                    }
+                }
             }
             System.out.println("");
         }
